222959ab0d82885d1f2e97fc0843331a2c8d3635
mersin shijo <mersinshijo@gmail.com>
2024-09-26 15:57:11 +0530

    Add files via upload

---
+++ nashath dom/script.js
@@ -0,0 +1,29 @@
+var hd =document.getElementById("head1");
+hd.style.color="blue"
+//hd.innertext="text changed";
+hd.innerHTML='<p>changed into p tag</p>'
+
+//using class name
+var user=document.getElementsByClassName("user");
+for(let i=0;i<user.length;i++){
+    user[i].style.color="red"
+}
+//using tag name
+var tag1=document.getElementsByTagName("li");
+tag1[2].style.backgroundColor="yellow";
+
+
+// using query slector using tagname
+var inp=document.querySelector('input');
+inp.placeholder="pling!!"
+
+//using query selector classname
+var qclass=document.querySelector(".qclass");
+qclass.style.backgroundColor="green"
+
+// query selector using id
+var btn= document.querySelector("#qid")
+btn.innerText="changed"
+
+
+
